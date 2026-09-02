f5e705cf32ef60bcb4d3f479dd2a4fa4b74873ca
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2026-09-02 22:05:39 +0700

    balance platform

---
+++ Assets/Scripts/Environment/BalancePlatform.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class BalancePlatform : MonoBehaviour
+{
+
+    [SerializeField] private float _restoreForce = 10f;
+
+    [SerializeField] private float _damping = 2f;
+
+
+    private Rigidbody2D _rigidbody2D;
+
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    public bool IsPlayerOnTop = false;
+
+
+    private void FixedUpdate()
+    {
+        if (!IsPlayerOnTop)
+        {
+            float currentAngle = Mathf.DeltaAngle(0, transform.eulerAngles.z);
+
+            float torque = -currentAngle * _restoreForce;
+
+            _rigidbody2D.AddTorque(torque);
+
+            _rigidbody2D.angularVelocity = Mathf.Lerp(_rigidbody2D.angularVelocity, 0, _damping * Time.fixedDeltaTime);
+
+
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.TryGetComponent(out Player player))
+        {
+            IsPlayerOnTop = true;
+          
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.TryGetComponent(out Player player))
+        {
+            IsPlayerOnTop = false;
+            
+        }
+    }
+}
